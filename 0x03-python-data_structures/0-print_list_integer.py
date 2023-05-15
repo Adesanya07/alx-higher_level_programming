1544226a06a7413ae9771c3cd9c55d24a132642f
Adesanya07 <gadesanya22@gmail.com>
2023-05-15 21:03:41 +0100

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""understanding and learning data structures in python"""
+"""author: GABRIEL ADESANYA (gadesanya22@gmail.com)"""
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
