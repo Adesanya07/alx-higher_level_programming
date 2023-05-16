a36e5236a7f6de447f95a499472c812cc5f9ea1d
Adesanya07 <gadesanya22@gmail.com>
2023-05-16 12:10:22 +0100

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    for i in sentence:
+        if sentence == 0:
+            return (len(sentence), None)
+        else:
+            return (len(sentence), sentence[0])
