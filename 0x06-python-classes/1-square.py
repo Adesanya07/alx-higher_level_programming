0ba529089047f8ae19873f7dba994761c7765d4c
Adesanya07 <gadesanya22@gmail.com>
2023-06-23 20:19:34 +0100

    square1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+
+"""this is adding size to the class module."""
+
+class Square:
+    """i will create a function that includes size"""
+    def __init__(self, size):
+        self.__size = size
+        
