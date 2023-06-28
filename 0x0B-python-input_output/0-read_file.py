16b6890d0ffc7f56b046df036aef12f78348bd23
Adesanya07 <gadesanya22@gmail.com>
2023-06-28 08:26:26 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""opening a file """
+
+def read_file(filename=""):
+    """writing a function that opens a file"""
+    with open(filename, "r", encoding="UTF8") as file:
+        content = file.read()
+        print(content)
