9664e993eedf4af83fbdf47ed71492b7f92b5c10
Adesanya07 <gadesanya22@gmail.com>
2023-08-19 16:11:41 +0100

    argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function argument () {
+    if (argument.length === 0){
+        console.log("No argument");
+    }
+    else if (argument.length === 1){
+        console.log("Argument found");
+    }
+    else{
+        console.log("Argument found");
+    }
+}
