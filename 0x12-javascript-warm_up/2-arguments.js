b84a76c19788a5f755ab78b927e7f08cd32b6f47
Adesanya07 <gadesanya22@gmail.com>
2023-08-26 09:07:54 +0100

    error fixed in task 1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
 
-function printArguments () {
 
-  if (arguments.length === 0) {
-    console.log('No argument');
-  } else if (arguments.length === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
-}
+
+    const argCount = process.argv.length;
+    argCount <= 2 && console.log('No argument');
+    argCount === 3 && console.log('Argument found');
+    argCount > 3 && console.log('Arguments found');
+
