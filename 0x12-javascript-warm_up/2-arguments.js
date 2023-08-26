98e52d9af692a6f616ebe54095d319e85c8bb1e5
Adesanya07 <gadesanya22@gmail.com>
2023-08-26 08:56:36 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+
+function printArguments () {
+
+  if (arguments.length === 0) {
+    console.log('No argument');
+  } else if (arguments.length === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
